c2513c03c9d59fe8b388432349343b6e747f74bc
Tajveez <tajveez786@gmail.com>
2020-09-14 23:29:48 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Flashcard from "./components/Flashcard";
+import FlashcardList from "./components/FlashcardList";
 
 function App() {
   return (
     <div className="App">
+      <Flashcard />
+      <FlashcardList />
       <h1>Hello World</h1>
     </div>
   );
