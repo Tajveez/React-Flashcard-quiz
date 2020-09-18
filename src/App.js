821598be3b76e1281ac5e026c35305dc89e22467
Tajveez <tajveez786@gmail.com>
2020-09-18 11:41:42 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,57 +1,112 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import "./App.css";
 
 import FlashcardList from "./components/FlashcardList";
 
 function App() {
-  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+  //States
+  const [flashcards, setFlashcards] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  //Refs
+  const categoryEl = useRef();
+  const amountEl = useRef();
 
   useEffect(() => {
-    axios.get("https://opentdb.com/api.php?amount=10").then((res) => {
-      setFlashcards(
-        res.data.results.map((questionItem, index) => {
-          const answer = decodeString(questionItem.correct_answer);
-          const options = [
-            ...questionItem.incorrect_answers.map((a) => decodeString(a)),
-            answer,
-          ];
-          return {
-            id: `${index}-${Date.now()}`,
-            question: decodeString(questionItem.question),
-            answer: answer,
-            options: options.sort(() => Math.random - 0.5),
-          };
-        })
-      );
+    axios.get("https://opentdb.com/api_category.php").then((res) => {
+      setCategories(res.data.trivia_categories);
     });
+    getQuestions();
   }, []);
 
+  function getQuestions(params = { amount: 10 }) {
+    let url = "https://opentdb.com/api.php";
+    axios
+      .get(url, {
+        params,
+      })
+      .then((res) => {
+        setFlashcards(
+          res.data.results.map((questionItem, index) => {
+            const answer = decodeString(questionItem.correct_answer);
+            const options = [
+              ...questionItem.incorrect_answers.map((a) => decodeString(a)),
+              answer,
+            ];
+            return {
+              id: `${index}-${Date.now()}`,
+              question: decodeString(questionItem.question),
+              answer: answer,
+              options: options.sort(() => Math.random - 0.5),
+            };
+          })
+        );
+      });
+  }
   function decodeString(str) {
     const textArea = document.createElement("textarea");
     textArea.innerHTML = str;
     return textArea.value;
   }
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    getQuestions({
+      category: categoryEl.current.value,
+      amount: amountEl.current.value,
+    });
+  }
+
   return (
-    <div className="container">
-      <FlashcardList flashcards={flashcards} />
-    </div>
+    <>
+      <form className="header" onClick={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="category">Category</label>
+          <select id="category" ref={categoryEl}>
+            {categories.map((category) => {
+              return (
+                <option value={category.id} key={category.id}>
+                  {category.name}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">No of questions</label>
+          <input
+            type="number"
+            id="amount"
+            min="1"
+            step="1"
+            defaultValue={10}
+            ref={amountEl}
+          />
+        </div>
+        <div className="form-group">
+          <button className="btn">Generate</button>
+        </div>
+      </form>
+      <div className="container">
+        <FlashcardList flashcards={flashcards} />
+      </div>
+    </>
   );
 }
 
-const SAMPLE_FLASHCARDS = [
-  {
-    id: 1,
-    question: "Question 1?",
-    answer: "1",
-    options: ["answer 1", "answer 2", "answer 3", "answer 4"],
-  },
-  {
-    id: 2,
-    question: "Question 2?",
-    answer: "2",
-    options: ["answer 1", "answer 2", "answer 3", "answer 4"],
-  },
-];
+// const SAMPLE_FLASHCARDS = [
+//   {
+//     id: 1,
+//     question: "Question 1?",
+//     answer: "1",
+//     options: ["answer 1", "answer 2", "answer 3", "answer 4"],
+//   },
+//   {
+//     id: 2,
+//     question: "Question 2?",
+//     answer: "2",
+//     options: ["answer 1", "answer 2", "answer 3", "answer 4"],
+//   },
+// ];
 export default App;
--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -32,12 +32,16 @@ function Flashcard({ flashcard }) {
         {flashcard.question}
         <div className="flashcard-options">
           {flashcard.options.map((option) => {
-            return <div className="flashcard-option">{option}</div>;
+            return (
+              <div className="flashcard-option" key={option}>
+                {option}
+              </div>
+            );
           })}
         </div>
       </div>
       <div className="back" ref={backEl}>
-        {flashcard.answer}
+        <i>{flashcard.answer}</i>
       </div>
     </div>
   );
