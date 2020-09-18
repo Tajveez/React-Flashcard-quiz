10a4899ed5732fa44498c1bac4af54b5c3e3b7ea
Tajveez <tajveez786@gmail.com>
2020-09-18 10:32:03 +0500

    adding API connection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 import FlashcardList from "./components/FlashcardList";
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+
+  useEffect(() => {
+    axios.get("https://opentdb.com/api.php?amount=10").then((res) => {
+      setFlashcards(
+        res.data.results.map((questionItem, index) => {
+          const answer = decodeString(questionItem.correct_answer);
+          const options = [
+            ...questionItem.incorrect_answers.map((a) => decodeString(a)),
+            answer,
+          ];
+          return {
+            id: `${index}-${Date.now()}`,
+            question: decodeString(questionItem.question),
+            answer: answer,
+            options: options.sort(() => Math.random - 0.5),
+          };
+        })
+      );
+    });
+  }, []);
+
+  function decodeString(str) {
+    const textArea = document.createElement("textarea");
+    textArea.innerHTML = str;
+    return textArea.value;
+  }
+
   return (
-    <div className="App">
+    <div className="container">
       <FlashcardList flashcards={flashcards} />
     </div>
   );
--- src/components/FlashcardList.js
+++ src/components/FlashcardList.js
@@ -1,6 +1,12 @@
 import React from "react";
-import Flashcard from "./components/Flashcard";
+import Flashcard from "./Flashcard";
 
-export default function FlashcardList() {
-  return <div>flashcard list</div>;
+export default function FlashcardList({ flashcards }) {
+  return (
+    <div className="card-grid">
+      {flashcards.map((flashcard) => {
+        return <Flashcard flashcard={flashcard} key={flashcard.id} />;
+      })}
+    </div>
+  );
 }
